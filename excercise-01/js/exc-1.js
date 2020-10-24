a02597aba320a253693f16df489d5730a37be7a7
Lautaro Paciarotti <lautaro.paciarotti@hotmail.com>
2020-10-24 12:25:12 -0300

    PS-04: Ex 1-a

--- excercise-01/js/exc-1.js
+++ excercise-01/js/exc-1.js
@@ -1,2 +1,14 @@
 //Javascript excercises - PS-04
 
+//Excercise 1
+    //a
+        var a = 3;
+        var b = 5;
+        var c = a+b;
+    //b
+        var name = 'Lautaro';
+        var lastName = 'Paciarotti';
+        var idPerson = name + lastName;
+        
+        
+        console.log(idPerson);
