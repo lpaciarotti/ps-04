98c890789c726ffa4a462f6d07c5b8b4803aabb3
Lautaro Paciarotti <lautaro.paciarotti@hotmail.com>
2020-11-02 12:05:14 -0300

    PS-05: Validation functions for name and email aded

--- excercise-02/JS/validations.js
+++ excercise-02/JS/validations.js
@@ -1,1 +1,34 @@
-alert('Its wokiiiiiiinggggg');
+//VALIDATION FUNCTIONS
+
+//Name validation
+
+// var nameInput = 'Manuela Gómez' ;
+// console.log(nameInput);
+// console.log(nameInput.indexOf(' '));
+
+// if (nameInput.length<= 6 || nameInput.indexOf(' ')<=0) {
+//     alert('The name should have 6 or more characters and at least one space');
+// }
+
+//Email validation
+
+var emailInput = 'alguien@mail.com';
+var emailLength = emailInput.length;
+console.log(emailInput);
+
+var atExistent = 0;
+
+for (var i=0; i<=emailLength; i++) {
+    if (emailInput[i]=='@') {
+        atExistent++;
+    }
+}
+
+var dotCom = emailInput.substring(emailLength-4,emailLength);
+
+console.log(atExistent);
+console.log(dotCom);
+
+if ((atExistent==0 || atExistent > 1)||dotCom != '.com') {
+    alert('Insert a valid email format: soomebody@smth.com');
+}
