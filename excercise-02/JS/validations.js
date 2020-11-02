9b63bf32d263d2546b856360488af4d6db2d22a2
Lautaro Paciarotti <lautaro.paciarotti@hotmail.com>
2020-11-02 14:52:28 -0300

    PS-05: Password, age and telephone validations aded

--- excercise-02/JS/validations.js
+++ excercise-02/JS/validations.js
@@ -2,33 +2,94 @@
 
 //Name validation
 
-// var nameInput = 'Manuela Gómez' ;
-// console.log(nameInput);
-// console.log(nameInput.indexOf(' '));
-
-// if (nameInput.length<= 6 || nameInput.indexOf(' ')<=0) {
-//     alert('The name should have 6 or more characters and at least one space');
+// window.onload = function() {
+//     var nameInput = document.getElementsByClassName('form-item');
+//     nameInput = nameInput[0].value;
+// // var nameInput = 'Manuela Gómez' ;
+//  //console.log(nameInput);
+// // console.log(nameInput.indexOf(' '));
+//     if (nameInput.length<= 6 || nameInput.indexOf(' ')<=0) {
+//         alert('The name should have 6 or more characters and at least one space');
+//         }
 // }
 
 //Email validation
 
-var emailInput = 'alguien@mail.com';
-var emailLength = emailInput.length;
-console.log(emailInput);
+// var emailInput = 'alguien@mail.com';
+// var emailLength = emailInput.length;
+// console.log(emailInput);
 
-var atExistent = 0;
+// var atExistent = 0;
 
-for (var i=0; i<=emailLength; i++) {
-    if (emailInput[i]=='@') {
-        atExistent++;
+// for (var i=0; i<=emailLength; i++) {
+//     if (emailInput[i]=='@') {
+//         atExistent++;
+//     }
+// }
+
+// var dotCom = emailInput.substring(emailLength-4,emailLength);
+
+// console.log(atExistent);
+// console.log(dotCom);
+
+// if ((atExistent==0 || atExistent > 1)||dotCom != '.com') {
+//     alert('Insert a valid email format: soomebody@smth.com');
+// }
+
+//Password validation
+
+// var passInput ='abcd1234'
+// console.log(passInput);
+// var passLength = passInput.length;
+// console.log(passLength);
+
+// var numQty = 0;
+// var letQty = 0;
+// var othQty = -1;
+
+// for (var i=0; i<=passLength; i++) {
+//     if (passInput[i]>=0 && passInput[i]<=9) {
+//         numQty++;
+//     } else if ((passInput[i]>= 'a' && passInput[i]<= 'z')||(passInput[i]>= 'A' && passInput[i]<= 'Z')) {
+//         letQty++;
+//     } else {
+//         othQty++;
+//     }
+// }
+
+// console.log('numQty',numQty);
+// console.log('letQty',letQty);
+// console.log('othQty',othQty);
+// console.log(typeof(passInput[0]));
+// if (numQty+letQty<8||othQty>0){
+//     alert('Password should have at least 8 characters and just numbers and leters');
+// }
+
+// var rPassInput = 'abcd1234'
+// if (rPassInput!=passInput) {
+//     alert('The password doesn\'t match');
+// }
+
+//Age validation
+
+// var ageInput = 23;
+// if (!Number.isInteger(ageInput)||ageInput<18) {
+//     alert('Age must be an integer and more or equal to 18');
+// }
+
+//Telephone validation
+
+var telephoneInput='4483235';
+var telephoneLength = telephoneInput.length;
+var wrngChar = 0;
+console.log(telephoneLength);
+
+for (var i=0;i<=telephoneLength;i++) {
+    if (telephoneInput[i]==' '||telephoneInput[i]=='-'||telephoneInput[i]=='('||telephoneInput[i]==')') {
+        wrngChar++;
     }
 }
 
-var dotCom = emailInput.substring(emailLength-4,emailLength);
-
-console.log(atExistent);
-console.log(dotCom);
-
-if ((atExistent==0 || atExistent > 1)||dotCom != '.com') {
-    alert('Insert a valid email format: soomebody@smth.com');
+if (wrngChar>0||telephoneLength<7) {
+    alert('Telephone must have at least 7 characters and not cointain "(" ")" "-" or spaces');
 }
