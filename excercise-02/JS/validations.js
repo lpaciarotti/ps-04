b32695e4fa1bd17b8d61b08b6896568c12a4ef0d
Lautaro Paciarotti <lautaro.paciarotti@hotmail.com>
2020-11-03 11:15:00 -0300

    PS-05: README.md file modified

--- excercise-02/JS/validations.js
+++ excercise-02/JS/validations.js
@@ -1,10 +1,8 @@
 //VALIDATION FUNCTIONS
-
 window.onload = function() {
     var form = document.querySelector('form');
     var submit = document.getElementsByClassName('submit-btn');
     var para = document.querySelector('p');
-
     //Name validation
     var nameInput = document.getElementById('name-item'); 
     var message = document.getElementById('message');
@@ -17,8 +15,6 @@ window.onload = function() {
     nameInput.onfocus = function(e) {
         message.textContent=' ';
     }
-    
-
     //Email validation
     var emailInput = document.getElementById('email-item');
     var emailMessage = document.getElementById('email-message');
@@ -31,15 +27,13 @@ window.onload = function() {
             }
         }
         var dotCom = emailInput.value.substring(emailLength-4,emailLength);
-
         if ((atExistent==0 || atExistent > 1)||dotCom != '.com') {
-            emailMessage.textContent = '\n'+'- Insert a valid email format: soomebody@smth.com';
+            emailMessage.textContent = '\n' + '- Insert a valid email format: soomebody@smth.com';
         }
     }
     emailInput.onfocus = function(e) {
         emailMessage.textContent=' ';
     }
-
     //Password validation
     var passInput = document.getElementById('pass-item');
     var passMessage = document.getElementById('pass-message');
@@ -58,16 +52,15 @@ window.onload = function() {
             }
         }
         if (numQty==0||letQty==0||numQty+letQty<8||othQty>0){
-            passMessage.textContent = '\n'+'- Password must have at least 8 characters and just numbers and leters';
+            passMessage.textContent = '\n' + '- Password must have at least 8 characters and just numbers and leters';
         }
     }
     passInput.onfocus = function(e) {
         passMessage.textContent=' ';
     }
-
     //Repeat password validation
-        var rPassInput = document.getElementById('rpass-item');
-        var rpassMessage = document.getElementById('rpass-message');
+    var rPassInput = document.getElementById('rpass-item');
+    var rpassMessage = document.getElementById('rpass-message');
     rPassInput.onblur = function(e) {
         if (rPassInput.value!=passInput.value) {
             rpassMessage.textContent = '- The password doesn\'t match';
@@ -76,74 +69,64 @@ window.onload = function() {
     rPassInput.onfocus = function(e) {
         rpassMessage.textContent =' ';
     }
-
     //Age validation
     var ageInput = document.getElementById('age-item');
     var ageMessage = document.getElementById('age-message');
     ageInput.onblur = function(e) {
         ageNumber = parseFloat(ageInput.value);
         if (!(Number.isInteger(ageNumber))||ageNumber<18) {
-        ageMessage.textContent = '\n'+'- Age must be an integer and more or equal to 18'; 
+        ageMessage.textContent = '\n' + '- Age must be an integer and more or equal to 18'; 
        }
     }
     ageInput.onfocus = function(e) {
         ageMessage.textContent = ' ';
     }
-
-    
     //Telephone validation
-
     var telephoneInput = document.getElementById('telephone-item');
     var wrngChar = 0;
     var telMessage = document.getElementById('tel-message');
     telephoneInput.onblur = function(e) {
         var telephoneLength = telephoneInput.value.length;
-        console.log(telephoneLength);
-
         for (var i=0;i<=telephoneLength;i++) {
             if (telephoneInput.value[i]==' '||telephoneInput.value[i]=='-'||telephoneInput.value[i]=='('||telephoneInput.value[i]==')') {
                 wrngChar++;
             }
         }
-
         if (wrngChar>0||telephoneLength<7) {
-            telMessage.textContent = '\n'+'- Telephone must have at least 7 characters and not cointain "(" ")" "-" or spaces';
+            telMessage.textContent = '\n' + '- Telephone must have at least 7 characters and not cointain "(" ")" "-" or spaces';
         }
     }
     telephoneInput.onfocus = function(e) {
         telMessage.textContent = ' ';
     }
-
     //Adress Validation
     var adressInput = document.getElementById('adress-item');
     var adressMessage = document.getElementById('adress-message');
     adressInput.onblur = function(e) {
         if (adressInput.value.length< 5 || adressInput.value.indexOf(' ')<=0) {
-            adressMessage.textContent = '\n'+'- Adress must have 5 or more characters and at least one space';
-            }    
+            adressMessage.textContent = '\n' + '- Adress must have 5 or more characters and at least one space';
+            }
     }
     adressInput.onfocus = function(e) {
         adressMessage.textContent = ' ';
     }
-
     //City validation
     var cityInput = document.getElementById('city-item');
     var cityMessage = document.getElementById('city-message');
     cityInput.onblur = function(e) {
         if (cityInput.value.length<3) {
-            cityMessage.textContent = '\n'+'- City must have at least 3 characters';
+            cityMessage.textContent = '\n' + '- City must have at least 3 characters';
         }
     }
     cityInput.onfocus = function(e) {
         cityMessage.textContent = ' ';
     }
-
     //ZIP Code validation
     var zipInput = document.getElementById('zip-item');
     var zipMessage = document.getElementById('zip-message');
     zipInput.onblur = function(e) {
         if (zipInput.value.length<3) {
-            zipMessage.textContent = '\n'+'- ZIP code must have at least 3 characters';
+            zipMessage.textContent = '\n' + '- ZIP code must have at least 3 characters';
         }
     }
     zipInput.onfocus = function(e) {
@@ -154,53 +137,20 @@ window.onload = function() {
     var dniMessage = document.getElementById('dni-message');
     dniInput.onblur = function(e) {
         if (dniInput.value.length<7||dniInput.value.length>8) {
-            dniMessage.textContent = '\n'+'- DNI must have 7 or 8 characters';
+            dniMessage.textContent = '\n' + '- DNI must have 7 or 8 characters';
         }
     }
     dniInput.onfocus = function(e) {
         dniMessage.textContent = ' ';
     }
-    
-
+    //Validations when submit button is clicked
     form.onsubmit = function(e) {
-
+        //No wrong data
         if (message.textContent == ' ' && emailMessage.textContent == ' ' && passMessage.textContent == ' ' &&rpassMessage.textContent == ' ' && ageMessage.textContent == ' ' && telMessage.textContent == ' ' && adressMessage.textContent == ' ' && cityMessage.textContent == ' ' && zipMessage.textContent == ' ' && dniMessage.textContent ==' ') {
-                alert('Information loaded succesfully:'+'\n'+'Name: '+nameInput.value+'\n'+'Email: '+emailInput.value+'\n'+'Password: '+passInput.value+'\n'+'Age: '+ageInput.value+'\n'+'Telephone: '+telephoneInput.value+'\n'+'Adress: '+adressInput.value+'\n'+'City: '+cityInput.value+'\n'+'ZIP Code: '+ zipInput.value+'\n'+'DNI: '+dniInput.value);
-            } else {
-            alert('One or more fields have errors:'+'\n'+ message.textContent+
-            emailMessage.textContent+passMessage.textContent+rpassMessage.textContent+ageMessage.textContent+telMessage.textContent+adressMessage.textContent+cityMessage.textContent+zipMessage.textContent+dniMessage.textContent);
+                alert('Information loaded succesfully:' + '\n' + 'Name: ' + nameInput.value + '\n' + 'Email: '+ emailInput.value + '\n' + 'Password: ' + passInput.value + '\n' + 'Age: ' + ageInput.value + '\n'+ 'Telephone: ' + telephoneInput.value + '\n' + 'Adress: ' + adressInput.value + '\n' + 'City: ' + cityInput.value + '\n' + 'ZIP Code: ' + zipInput.value + '\n' + 'DNI: ' + dniInput.value);
+        } else {
+            alert('One or more fields have errors:' + '\n' + message.textContent +
+            emailMessage.textContent + passMessage.textContent + rpassMessage.textContent + ageMessage.textContent + telMessage.textContent + adressMessage.textContent + cityMessage.textContent + zipMessage.textContent +  dniMessage.textContent);
         }
     }
-
-        // if(message.textContent!=' '){
-        //     alert('The name must have at least 6 characters and one space');
-        // }
-        // if(emailMessage.textContent!=' '){
-        //     alert('Insert a valid email format: soomebody@smth.com');
-        // }
-        // if(passMessage.textContent!=' '){
-        //     alert('Password must have at least 8 characters and just numbers and leters');
-        // }
-        // if(rpassMessage.textContent!=' '){
-        //     alert('The password doesn\'t match');
-        // }
-        // if(ageMessage.textContent!=' '){
-        //     alert('Age must be an integer and more or equal to 18');
-        // }
-        // if(telMessage.textContent!=' '){
-        //     alert('Telephone must have at least 7 characters and not cointain "(" ")" "-" or spaces');
-        // }
-        // if(adressMessage.textContent!=' '){
-        //     alert('Adress must have 5 or more characters and at least one space');
-        // }
-        // if(cityMessage.textContent!=' '){
-        //     alert('City must have at least 3 characters');
-        // }
-        // if(zipMessage.textContent!=' '){
-        //     alert('ZIP code must have at least 3 characters');
-        // }
-        // if(dniMessage.textContent!=' '){
-        //     alert('DNI must have 7 or 8 characters');
-        // }
-        //  }
 }
--- excercise-02/README.md
+++ excercise-02/README.md
@@ -1,3 +1,3 @@
-In this work you will see the same homepage of a newspaper done by me (Lautaro Paciarotti) in the PS-03, but now it is corrected acording to the tutor advices.
+In this work you will see the same homepage of a newspaper done by me (Lautaro Paciarotti) in the PS-03, corrected acording to the tutor advices and now it also has a subscription form with all the settings done in JavaScript file "validation.js".
 On the following link you will see the website:
 https://lpaciarotti.github.io/ps-04/excercise-02/home.html
